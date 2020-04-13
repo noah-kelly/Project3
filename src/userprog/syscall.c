ecbf5d7650be42b3079e09aabe250de5d84df7ae
Noah Kelly <noahk@ksu.edu>
2020-04-12 19:27:46 -0500

    Most tests passing

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -504,7 +504,24 @@ lookup_mapping (int handle)
 static void
 unmap (struct mapping *m) 
 {
-/* add code here */
+
+  list_remove(&m->elem);
+
+  for (int i = 0; i < m->page_cnt; i++)
+  {
+	if (pagedir_is_dirty(thread_current()->pagedir, ((const void *) ((m->base) + (PGSIZE * i)))))
+	{
+		lock_acquire (&fs_lock);
+		file_write_at(m->file, (const void *) (m->base + (PGSIZE * i)), (PGSIZE*(m->page_cnt)), (PGSIZE * i));
+		lock_release (&fs_lock);
+	}
+  }
+
+  for (int i = 0; i < m->page_cnt; i++)
+  {
+     page_deallocate((void *) ((m->base) + (PGSIZE * i)));
+  }
+
 }
  
 /* Mmap system call. */
@@ -561,6 +578,9 @@ static int
 sys_munmap (int mapping) 
 {
 /* add code here */
+
+  struct mapping* map = lookup_mapping(mapping);
+  unmap(map);
 
   return 0;
 }
--- src/vm/page.c
+++ src/vm/page.c
@@ -54,7 +54,12 @@ page_for_addr (const void *address)
 
 /* add code */
 
-    }
+   if ((p.addr > PHYS_BASE - STACK_MAX) && ((void *)thread_current()->user_esp - 32 < address))
+   {
+	return page_allocate (p.addr, false);
+   }
+
+  }
   return NULL;
 }
 
@@ -145,15 +150,40 @@ page_out (struct page *p)
      dirty bit, to prevent a race with the process dirtying the
      page. */
 
-/* add code here */
+  pagedir_clear_page(p->thread->pagedir, (void *) p->addr);
 
   /* Has the frame been modified? */
 
-/* add code here */
+  dirty = pagedir_is_dirty (p->thread->pagedir, (const void *) p->addr);
 
-  /* Write frame contents to disk if necessary. */
+  if (!dirty)
+  {
+	ok = true;
+  }
 
-/* add code here */
+  if(p->file == NULL)
+  {
+	ok = swap_out(p);
+  }
+  else
+  {
+	if (dirty)
+	{
+		if (p->private)
+		{
+  		  ok = swap_out(p);
+		}
+		else
+		{
+		  ok = file_write_at(p->file, (const void *) p->frame->base, p->file_bytes, p->file_offset);
+		}
+	}
+  }
+
+  if (ok)
+  {
+	p->frame = NULL;
+  }
 
   return ok;
 }
